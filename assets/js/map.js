3fef57f0a69a7bce9509590ba5039e636edbd901
Rafalcho <rafalcho@github.com>
2017-04-29 23:33:53 +0200

    working click event and transport type

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,54 +1,56 @@
-
+var markersArray = [];
 
 function initMap() {
   var directionsDisplay = new google.maps.DirectionsRenderer;
   var directionsService = new google.maps.DirectionsService;
+
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 14,
+    zoom: 13,
     center: {lat: 52.229802, lng: 21.011818}
   });
+
   directionsDisplay.setMap(map);
-
-  calculateAndDisplayRoute(directionsService, directionsDisplay);
-  document.getElementById('mode').addEventListener('change', function() {
-    calculateAndDisplayRoute(directionsService, directionsDisplay);
-  });
-
-  var markersArray = [];
 
   map.addListener('click', function(event) {
     markersArray.map(marker => marker.setMap(null));
 
     var latitude = event.latLng.lat();
     var longitude = event.latLng.lng();
-    console.log(latitude);
 
     var marker = new google.maps.Marker({
       position: {lat: latitude, lng: longitude},
       map: map,
-      title: 'Click to zoom'
+
     });
 
     markersArray.push(marker);
 
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+
   });
+
+  document.getElementById('mode').addEventListener('change', function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  });
+
+  var onChangeHandler = function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+  document.getElementById('start').addEventListener('change', onChangeHandler);
+  document.getElementById('end').addEventListener('change', onChangeHandler);
 }
 
 function calculateAndDisplayRoute(directionsService, directionsDisplay) {
   var selectedMode = document.getElementById('mode').value;
   directionsService.route({
-    origin: {lat: 52.219827, lng: 21.018017},  // Haight.
-    destination: {lat: 52.238143, lng: 21.051684},  // Ocean Beach.
-    // Note that Javascript allows us to access the constant
-    // using square brackets and a string value as its
-    // "property."
-    travelMode: google.maps.TravelMode[selectedMode]
+    origin: markersArray.length === 0 ? {lat: 52.219827, lng: 21.018017} : markersArray[markersArray.length - 1].position,
+    destination: {lat: 52.229802, lng: 21.011818},
+    travelMode: selectedMode
   }, function(response, status) {
-  if (status == 'OK') {
-  directionsDisplay.setDirections(response);
-  } else {
-  window.alert('Directions request failed due to ' + status);
-  }
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
   });
-
 }
