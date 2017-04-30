b90ad64d4437867f920cde03f6cc5e5f48de7964
kikarto <kikarto@gmail.com>
2017-04-30 14:11:28 +0200

    script offer

--- assets/js/show_profile_and_offer.js
+++ assets/js/show_profile_and_offer.js
@@ -1,6 +1,6 @@
 var url = "http://192.168.43.49:8000/";
 
-$("body").on("click", "", function(e){
+$("body").on("click", "#test_01", function(e){
 	
 	var id = 1;
 	
@@ -44,7 +44,7 @@ $("body").on("click", "", function(e){
 	
 });
 
-$("body").on("click", "", function(e){
+$("body").on("click", "#test_02", function(e){
 	
 	var id = 1;
 	
@@ -56,26 +56,17 @@ $("body").on("click", "", function(e){
 			//	user
 				var el = $("#Profile");
 				var img = el.find("#user_photo");
-				var expirience = el.find("#expirience_cont");
-				var skills = el.find("#skills_cont");
-				var desc = el.find("#desc_cont");
-				var links = el.find("#links_cont");
+				var position = el.find("#offer_position_cont");
+				var salary = el.find("#offer_salary_cont");
+				var address = el.find("#offer_address_cont");
+				var desc = el.find("#offer_desc_cont");
 				var i;
 				
-				img.attr("src", url + "account/" + result.pictuer);
-				expirience.html("");
-				for(i = 0; i < result.expirience.length; i++){
-					expirience.append("<p>" + result.expirience[i].year + " - " + result.expirience[i].name + "</p>");
-				}
-				skills.html("");
-				for(i = 0; i < result.skills.length; i++){
-					skills.append("<p> - " + result.skills + "</p>");
-				}
+				img.attr("src", url + "offer/" + result.pictuer);
+				position.html(result.position);
+				salary.html(result.min_salary + " - " + result.max_salary);
+				address.html(result.address);
 				desc.html(result.desc);
-				links.html("");
-				for(i = 0; i < result.links.length; i++){
-					links.append("<p><span>icon</span><a href='" + result.links[i] + "'>" + result.links[i] + "</a></p>");
-				}
 				
 				el.show();
 			//}else{
